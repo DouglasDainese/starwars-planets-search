b62e9e8fea9705e9cf7d4d53d0c6cc467422feaf
DouglasDainese <douglas_dainese@hotmail.com>
2023-08-19 17:59:19 -0300

    Iniciando estilização

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Table from './components/Table';
 import Filter from './components/Filter';
+import logoImg from './img/logo-star-wars.png';
 
 function App() {
   return (
     <>
+      <div id="circle-1">
+        <div id="circle-2">
+          <img src={ logoImg } alt="logo StarWar" />
+        </div>
+      </div>
+
       <h1>Star Wars Planets</h1>
       <span>Hello, Padawans!</span>
       <Filter />
